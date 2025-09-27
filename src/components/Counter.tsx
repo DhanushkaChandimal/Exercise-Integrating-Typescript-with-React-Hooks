acb93c147f7005ea2e38a937290335b0cd302b8d
Dhanushka <dhanushkamcr@gmail.com>
2025-09-26 19:11:00 -0500

    Create Counter component and add to App

---
+++ src/components/Counter.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    return(
+        <div>{count}</div>
+    );
+}
+
+export default Counter;
