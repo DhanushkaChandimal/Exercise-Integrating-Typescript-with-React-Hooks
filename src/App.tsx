ae0cd26a92f959a57206f4f77070f03966409675
Dhanushka <dhanushkamcr@gmail.com>
2025-09-26 19:30:41 -0500

    Add reducer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Counter from './components/Counter'
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
 
   return (
     <>
       <Counter/>
+      <h1>Shopping Cart</h1>
+      <ShoppingCart />
     </>
   )
 }
